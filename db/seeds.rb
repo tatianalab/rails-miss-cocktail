3336a45ca55f1b304ea7a0e2ef3a313d883e18ef
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-11-12 13:47:39 -0500

    seeded the db-without json for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+puts 'Database is clean now 🍹'
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "cane syrup")
+Ingredient.create(name: "soda")
+Ingredient.create(name: "bitter")
+
+puts 'Ingredients are created...'
