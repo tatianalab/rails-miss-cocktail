02cddbd6ccd75c41222210aa3869ba4d37fcb421
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-11-13 14:40:23 -0500

    more style changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ root to: 'cocktails#index'
   resources :cocktails, except: [:destroy, :edit, :update] do
     resources :doses, only: [:index,:new, :create, :destroy] do
       resources :ingredients, only: [:index, :new, :create, :destroy]
