085b874e6e58c1638a7566382d0f607e8e775260
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-11-13 16:18:05 -0500

    added destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  root to: 'cocktails#index'
-  resources :cocktails, except: [:destroy, :edit, :update] do
+  resources :cocktails, except: [:edit, :update] do
     resources :doses, only: [:index,:new, :create, :destroy] do
       resources :ingredients, only: [:index, :new, :create, :destroy]
     end
